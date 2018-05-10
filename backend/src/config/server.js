0cd47d5df71ad650bcc181e06162b7259c608e06
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2018-05-10 14:05:24 -0300

    Termina configurações do backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,14 @@ const port = 3003;
 const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
+const allowsCors = require("./cors");
 
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true })); //Sempre que tiver server.use é um middle
 server.use(bodyParser.json());
+server.use(allowsCors);
 
 server.listen(port, function(){
     console.log(`BackEnd rodando na porta ${port}.`);
 })
+
+module.exports = server;
