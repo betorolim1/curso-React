e902397d241997bea9b358a8b83dca4cbc3bc651
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2018-05-11 15:02:19 -0300

    Continua o curso

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import PageHeader from "../template/pageHeader"
+
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum</p>
+        <h2>Missão</h2>
+        <p>Lorem</p>
+    </div>
+)
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,40 @@
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+ 
 module.exports = {
-    entry: "./src/index.jsx",
+    entry: './src/index.jsx',
     output: {
-        path: __dirname + "/public",
-        filename: "./app.js"
+        path: __dirname + '/public',
+        filename: './app.js'
     },
-    devServer:{
-        port: 8080,
-        contentBase: "./public",
+    devServer: {
+        port: 8008,
+        contentBase: './public'
     },
     resolve: {
-        extensions: ["", ".js", ".jsx"],
+        extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + "/node-modules"
+            modules: __dirname + '/node_modules'
         }
     },
-    plugin: [
-        new ExtractTextPlugin("app.css")
+    plugins: [
+        new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-            text: /.js[x]?$/,
-            loader: "babel-loader",
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ["es2015", "react"],
-                plugins: ["transform-object-rest-spread"]
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-            loader: "file"
+            loader: 'file'
         }]
-    }
+    } 
 }
