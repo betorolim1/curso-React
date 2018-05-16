aa7588876abdf1304618795d2433c15bde4eabe6
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2018-05-16 15:08:04 -0300

    Continua curso

--- Modulo React/ex.12/index.jsx
+++ Modulo React/ex.12/index.jsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from "redux"
 import { Provider } from "react-redux"
 
-import CounterReducer from "./counterReducer"
-import Counter from "./counter"
+import Field from './field'
 
 const reducers = combineReducers({
-    counter: CounterReducer
+    field: () => ({ value: "Opa" })
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)} >
-        <Counter />
+        <Field initialValue="Teste" />
     </Provider>
     , document.getElementById('app'))
